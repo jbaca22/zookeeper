394bed4519d83fd4850a16767023d05fef938470
Jarred Baca <jarredbaca@MacBook-Pro.local>
2020-07-19 11:14:08 -0600

    saving port constant

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const { animals } = require('./data/animals');
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 function filterByQuery(query, animalsArray) {
@@ -40,6 +41,7 @@ function filterByQuery(query, animalsArray) {
   // return the filtered results:
   return filteredResults;
 }
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -48,7 +50,7 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
 });
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
 });
 
